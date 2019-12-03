5eb81ea04374af299767c59422029a1ede3f742e
Alfred Zhong <alfred@cs.utexas.edu>
2019-12-02 18:47:52 -0600

    finished menu

--- source/scenes/menu.js
+++ source/scenes/menu.js
@@ -20,10 +20,21 @@ export class Menu extends Phaser.Scene{
         let playButton = this.add.image(350, 350, 'play-button').setInteractive();
         playButton.on("pointerup", ()=>{
             console.log("Starting Scrabble")
+            let num_receives = document.getElementById("numreceives").value
+            let num_sends = document.getElementById("numsends").value
+            console.log("nr = " + num_receives)
+            console.log("ns = " + num_sends)
+            let mrs = document.getElementById("mrs").checked
+            console.log("mrs = " + mrs)
+
+
             this.scene.start(CONSTANTS.SCENES.SCRABBLE);
+            document.getElementById("checkpoints").textContent = "it works"
+        
+
         })
 
-        document.getElementById("checkpoints").textContent = "it works"
+
 
         // var element = this.add.dom(-20, -20).createFromCache('inputform');
         // console.log('Dom Element:', element);
